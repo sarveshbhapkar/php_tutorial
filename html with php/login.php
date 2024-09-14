c0b834dfbb51b749fd379bff5314bf3849ac3344
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-09-14 10:25:36 +0530

    htmlwithphp

---
+++ html with php/login.php
@@ -0,0 +1,16 @@
+<?php
+
+
+// Send data with GET request |
+
+// « Make HTML file
+// « Make PHP file
+// + Make input field in HTML file £
+// « Receive data in php file
+
+echo "user name is ".$_GET['user_name'];
+echo "<br>";
+echo "user password is ".$_GET['user_password'];
+
+
+?>
